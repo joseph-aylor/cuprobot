4a293252cf6ff05a2ed9bd8f09e84de422f29a55
unknown <aylor@JAYLORL.empowermm.com>
2014-02-25 08:29:29 -0500

    forgot things

---
+++ routes/recipes.js
@@ -0,0 +1,6 @@
+
+exports.index = function(req, res){res.send("OHNO");}
+exports.add = function(req, res){res.send("OHNO");}
+exports.fetch = function(req, res){res.send("OHNO");}
+exports.update = function(req, res){res.send("OHNO");}
+exports.delete = function(req, res){res.send("OHNO");}
